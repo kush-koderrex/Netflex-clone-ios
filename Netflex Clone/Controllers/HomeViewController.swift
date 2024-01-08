fc9ff636c0739dbdabc4c18b7a1474db00bcfed2
kush-koderrex <kushkumar89792666@gmail.com>
2024-01-08 12:27:26 +0530

    Add navigation

--- Netflex Clone/Controllers/HomeViewController.swift
+++ Netflex Clone/Controllers/HomeViewController.swift
@@ -7,15 +7,52 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate {
+    
+    private let homeFeedTable : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+        
+
+    
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame=view.bounds
     }
 
+}
 
+extension HomeViewController:UIWebViewDelegate ,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for:indexPath)
+        cell.textLabel?.text = "Hello World"
+        cell.backgroundColor = .red
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
 }
